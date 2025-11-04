3fecba311f62dbed7a7189589b14e85847eec162
leonelpy2001 <124599074+leonelpy2001@users.noreply.github.com>
2025-11-04 21:39:13 +0100

    mostrar notificaoes abilitado

---
+++ js/notificacoes.js
@@ -0,0 +1,31 @@
+
+
+const mostrarNotificaoes = function () {
+
+    const mostrarNoty = function () {
+        const noty = new Notification("Gospress", {
+            body: "Voo com origem Huambo chegou a nossa estação!",
+            icon: "../imgs/logo/gospress_logo-removebg-preview.png",
+            requireInteraction: "ver"
+        })
+        return noty
+    }
+
+    const permissao = function (params) {
+
+        if (Notification.permission === "granted") {
+            mostrarNoty()
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(permission => {
+                if (permission === "granted") {
+                    mostrarNoty()
+
+                }
+            })
+        }
+
+    }
+    permissao()
+
+}
+mostrarNotificaoes()
