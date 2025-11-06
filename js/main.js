9ec2d3aa205742d334bfb5bb89f685f8367f5b7c
leonelpy2001 <124599074+leonelpy2001@users.noreply.github.com>
2025-11-06 10:46:35 +0100

    confercao de pwa abilitada

--- js/main.js
+++ js/main.js
@@ -145,7 +145,9 @@ VerificarLargura()
 setInterval(VerificarLargura, 1000)
 
 
-
-
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('Service Worker registrado com sucesso!'))
+    .catch(err => console.log('Erro ao registrar SW:', err));
+}
 
--- js/service-worker.js
+++ js/service-worker.js
@@ -6,3 +6,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('Service Worker ativo!');
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
